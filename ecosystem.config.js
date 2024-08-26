930d637da7acf159ef088bb7856d37057428334d
Tien Ly <tien.ly@terralogic.com>
2024-08-26 18:10:04 +0700

    add ecosytem

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: "HrosDev",
+      exec_mode: 'cluster',
+      instances: 'max',
+      script: 'node_modules/next/dist/bin/next',
+      args: 'start',
+      env_local: {
+        NEXT_PUBLIC_API_KEY: '4CB8B3EE874A94E9F8EDEC369A9E4'
+      }
+    }
+  ]
+}
